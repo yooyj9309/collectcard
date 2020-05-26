d0555f12a5e376847bad4ec0db34a825d495b65e
YeSangKug <54267881+YeSangKug@users.noreply.github.com>
2020-05-26 13:44:06 +0900

    Feature/init (#1)
    * Add git ignore
    
    * Init kotlin spring project
    
    * Add codeowners

---
+++ src/main/kotlin/com/rainist/collectcard/CollectcardApplication.kt
@@ -0,0 +1,11 @@
+package com.rainist.collectcard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CollectcardApplication
+
+fun main(args: Array<String>) {
+    runApplication<CollectcardApplication>(*args)
+}
---
+++ src/test/kotlin/com/rainist/collectcard/CollectcardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rainist.collectcard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CollectcardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
