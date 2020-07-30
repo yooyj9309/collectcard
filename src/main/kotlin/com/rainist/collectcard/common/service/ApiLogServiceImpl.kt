a0bf22eca8d5a24e168a490438391e117bdbd26d
Jayden <gwanggi.lee@gmail.com>
2020-07-30 13:49:31 +0900

    Fix logResponse function (#132)
    * Fix logResponse
    
    * Fix lint error

--- src/main/kotlin/com/rainist/collectcard/common/service/ApiLogServiceImpl.kt
+++ src/main/kotlin/com/rainist/collectcard/common/service/ApiLogServiceImpl.kt
@@ -15,46 +15,48 @@ class ApiLogServiceImpl(private val apiLogRepository: ApiLogRepository) : ApiLog
 
     override fun logRequest(organizationId: String, banksaladUserId: Long, apiLog: ApiLog) {
         apiLogRepository.save(
-            ApiLogEntity().apply {
-                this.requestId = apiLog.id
-                this.organizationId = organizationId
-                this.banksaladUserId = banksaladUserId
+                ApiLogEntity().apply {
+                    this.requestId = apiLog.id
+                    this.organizationId = organizationId
+                    this.banksaladUserId = banksaladUserId
 
-                this.apiId = apiLog.api.id
-                this.requestUrl = apiLog.api.endpoint
-                this.httpMethod = apiLog.api.method.name
-                this.organizationApiId = apiLog.api.name
+                    this.apiId = apiLog.api.id
+                    this.requestUrl = apiLog.api.endpoint
+                    this.httpMethod = apiLog.api.method.name
+                    this.organizationApiId = apiLog.api.name
 
-                this.requestHeaderJson = apiLog.request.header
-                this.requestBodyJson = apiLog.request.body
+                    this.requestHeaderJson = apiLog.request.header
+                    this.requestBodyJson = apiLog.request.body
 
-                this.transformedRequestHeaderJson = apiLog.request.transformedHeader
-                this.transformedRequestBodyJson = apiLog.request.transformedBody
+                    this.transformedRequestHeaderJson = apiLog.request.transformedHeader
+                    this.transformedRequestBodyJson = apiLog.request.transformedBody
 
-                this.requestDatetime = LocalDateTime.now(ZoneId.of(ZONE_ID_UTC))
-            }
+                    this.requestDatetime = LocalDateTime.now(ZoneId.of(ZONE_ID_UTC))
+                }
         )
     }
 
     override fun logResponse(organizationId: String, banksaladUserId: Long, apiLog: ApiLog) {
         val apiLogEntity = apiLogRepository.findByRequestId(apiLog.id)
-            ?: ApiLogEntity().apply {
-                this.organizationId = organizationId
-                this.banksaladUserId = banksaladUserId
-                this.apiId = apiLog.api.id
-                this.requestUrl = apiLog.api.endpoint
-                this.httpMethod = apiLog.api.method.name
-                this.organizationApiId = apiLog.api.name
-            }
+                ?: ApiLogEntity().apply {
+                    this.organizationId = organizationId
+                    this.banksaladUserId = banksaladUserId
+                    this.apiId = apiLog.api.id
+                    this.requestUrl = apiLog.api.endpoint
+                    this.httpMethod = apiLog.api.method.name
+                    this.organizationApiId = apiLog.api.name
+                }
 
         apiLogEntity.apply {
             this.responseCode = apiLog.response.responseCode
+            // TODO jayden-lee resultCode java-banksalad Response 객체 프로퍼티에 추가해야함
+            this.resultCode = ""
 
             this.responseHeaderJson = apiLog.response.header
             this.responseBodyJson = apiLog.response.body
 
-            this.responseHeaderJson = apiLog.response.transformedHeader
-            this.responseBodyJson = apiLog.response.transformedBody
+            this.transformedResponseHeaderJson = apiLog.response.transformedHeader
+            this.transformedResponseBodyJson = apiLog.response.transformedBody
 
             this.responseDatetime = LocalDateTime.now(ZoneId.of(ZONE_ID_UTC))
         }
