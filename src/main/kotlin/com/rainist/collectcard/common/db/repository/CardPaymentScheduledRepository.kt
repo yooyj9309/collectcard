2d7cdbea796903a890097b6dd18e4bc7d0ccb751
dusang <pesang72@hanmail.net>
2020-08-04 12:15:02 +0900

    modify delete query (#146)
    Co-authored-by: pesang72 <pesang72@hanail.net>

--- src/main/kotlin/com/rainist/collectcard/common/db/repository/CardPaymentScheduledRepository.kt
+++ src/main/kotlin/com/rainist/collectcard/common/db/repository/CardPaymentScheduledRepository.kt
@@ -2,8 +2,13 @@ package com.rainist.collectcard.common.db.repository
 
 import com.rainist.collectcard.common.db.entity.CardPaymentScheduledEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 
 interface CardPaymentScheduledRepository : JpaRepository<CardPaymentScheduledEntity, Long> {
+
+    @Modifying
+    @Query("delete from CardPaymentScheduledEntity t where t.banksaladUserId = ?1 and t.cardCompanyId = ?2")
     fun deleteAllByBanksaladUserIdAndCardCompanyId(
         banksaladUserId: Long?,
         cardCompanyId: String?
