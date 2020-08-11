2d00ef6fc58849f14d7bbb3ee468fe6ffb2ff997
Sangmin In <me@sangmin.in>
2020-08-11 11:46:26 +0900

    Fix Loan Diff (#185)
    * fix loan diff
    
    * lint
    
    * Revert "lint"
    
    This reverts commit ac95b84be043cd60eac6f65d646d151694b0c51f.
    
    * Revert "fix loan diff"
    
    This reverts commit 821e4f91777c4aada07f34fba59ac4f80cb305ce.
    
    * parse interest rate jslt in jslt
    
    * fix issued, expiration date format
    
    * lint
    
    * fix jslt parse zerofill number function
    
    * make parse zfill number safe

--- src/main/kotlin/com/rainist/collectcard/cardloans/dto/ListLoansResponse.kt
+++ src/main/kotlin/com/rainist/collectcard/cardloans/dto/ListLoansResponse.kt
@@ -3,6 +3,7 @@ package com.rainist.collectcard.cardloans.dto
 import com.github.rainist.idl.apis.v1.collectcard.CollectcardProto
 import com.google.protobuf.StringValue
 import com.rainist.common.exception.ValidationException
+import com.rainist.common.util.DateTimeUtil
 
 data class ListLoansResponse(
     var dataHeader: ListLoansResponseDataHeader? = null,
@@ -23,17 +24,26 @@ data class ListLoansResponseDataBody(
 fun ListLoansResponse.toListCardLoansResponseProto(): CollectcardProto.ListCardLoansResponse {
     return let {
         this.dataBody?.loans?.map { loan ->
-            CollectcardProto.CardLoan.newBuilder()
-                .setNumber(loan.loanNumber)
-                .setName(loan.loanName)
-                .setPrincipal(loan.loanAmount?.toInt() ?: throw ValidationException("대출 금액이 없습니다"))
-                .setCurrency("KRW") // TODO 예상국 기존 로직 확인 통화코드를 안줌
-                .setLatestBalance(loan.remainingAmount?.toInt() ?: throw ValidationException("대출 잔액이 없습니다"))
-                .setInterestRate(loan.interestRate?.toDouble() ?: throw ValidationException("이자율이 없습니다"))
-                .setCreatedAt(loan.issuedDay?.let { StringValue.of(it) } ?: StringValue.getDefaultInstance())
-                .setExpiredAt(loan.expirationDay?.let { StringValue.of(it) } ?: StringValue.getDefaultInstance())
-//                 .setOverdueStatus() // TODO 연체 여부
-                .build()
+            val loanBuilder = CollectcardProto.CardLoan.newBuilder()
+
+            loanBuilder.number = loan.loanNumber
+            loanBuilder.name = loan.loanName
+            loanBuilder.currency = "KRW"
+
+            loan.loanAmount?.let { loanBuilder.principal = loan.loanAmount?.toInt() ?: throw ValidationException("대출 금액이 없습니다") }
+            loan.remainingAmount?.let { loanBuilder.latestBalance = loan.remainingAmount?.toInt() ?: throw ValidationException("대출 잔액이 없습니다") }
+            loan.interestRate?.let { loanBuilder.interestRate = loan.interestRate?.toDouble() ?: throw ValidationException("이자율이 없습니다") }
+
+            loan.issuedDay?.let {
+                val issuedDate = DateTimeUtil.stringToLocalDate(it, "yyyyMMdd")
+                loanBuilder.createdAt = StringValue.of(DateTimeUtil.localDateToString(issuedDate, "yyyy-MM-dd"))
+            }
+            loan.expirationDay?.let {
+                val expiredDate = DateTimeUtil.stringToLocalDate(it, "yyyyMMdd")
+                loanBuilder.expiredAt = StringValue.of(DateTimeUtil.localDateToString(expiredDate, "yyyy-MM-dd"))
+            }
+
+            loanBuilder.build()
         }
         ?.toMutableList()
         ?: mutableListOf()
