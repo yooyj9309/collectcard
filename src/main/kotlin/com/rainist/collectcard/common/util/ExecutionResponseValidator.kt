bcb081568448ae28f3a638710352cbe495c394c4
WonJoon Choi <bimong@gmail.com>
2020-08-14 00:29:42 +0900

    Avoid throwing validation fail temporarly (#215)

--- src/main/kotlin/com/rainist/collectcard/common/util/ExecutionResponseValidator.kt
+++ src/main/kotlin/com/rainist/collectcard/common/util/ExecutionResponseValidator.kt
@@ -2,33 +2,38 @@ package com.rainist.collectcard.common.util
 
 import com.rainist.collect.common.execution.ExecutionResponse
 import com.rainist.collectcard.common.enums.ResultCode
-import com.rainist.collectcard.common.exception.CollectcardException
+import com.rainist.common.log.Log
 
 class ExecutionResponseValidator {
 
-    companion object {
+    companion object : Log {
         fun validateResponseAndThrow(
             executionResponse: ExecutionResponse<*>,
             resultCodes: List<ResultCode>
         ) {
             if (executionResponse.isExceptionOccurred) {
-                throw CollectcardException(ResultCode.UNKNOWN.name, ResultCode.UNKNOWN.name)
+//                throw CollectcardException(ResultCode.UNKNOWN.name, ResultCode.UNKNOWN.name)
+                logger.error("Response validation exception. currently just logging : {} ", ResultCode.UNKNOWN.name)
             }
 
             if (resultCodes.contains(ResultCode.EXTERNAL_SERVER_ERROR)) {
-                throw CollectcardException(ResultCode.EXTERNAL_SERVER_ERROR.name, ResultCode.EXTERNAL_SERVER_ERROR.name)
+//                throw CollectcardException(ResultCode.EXTERNAL_SERVER_ERROR.name, ResultCode.EXTERNAL_SERVER_ERROR.name)
+                logger.error("Response validation exception. currently just logging : {} ", ResultCode.EXTERNAL_SERVER_ERROR.name)
             }
 
             if (resultCodes.contains(ResultCode.INVALID_ACCESS_TOKEN)) {
-                throw CollectcardException(ResultCode.INVALID_ACCESS_TOKEN.name, ResultCode.INVALID_ACCESS_TOKEN.name)
+//                throw CollectcardException(ResultCode.INVALID_ACCESS_TOKEN.name, ResultCode.INVALID_ACCESS_TOKEN.name)
+                logger.error("Response validation exception. currently just logging : {} ", ResultCode.INVALID_ACCESS_TOKEN.name)
             }
 
             if (resultCodes.contains(ResultCode.INVALID_USER)) {
-                throw CollectcardException(ResultCode.INVALID_USER.name, ResultCode.INVALID_USER.name)
+//                throw CollectcardException(ResultCode.INVALID_USER.name, ResultCode.INVALID_USER.name)
+                logger.error("Response validation exception. currently just logging : {} ", ResultCode.INVALID_USER.name)
             }
 
             if (resultCodes.contains(ResultCode.UNKNOWN)) {
-                throw CollectcardException(ResultCode.UNKNOWN.name, ResultCode.UNKNOWN.name)
+//                throw CollectcardException(ResultCode.UNKNOWN.name, ResultCode.UNKNOWN.name)
+                logger.error("Response validation exception. currently just logging : {} ", ResultCode.UNKNOWN.name)
             }
         }
 
@@ -37,23 +42,28 @@ class ExecutionResponseValidator {
             resultCode: ResultCode?
         ) {
             if (executionResponse.isExceptionOccurred) {
-                throw CollectcardException(ResultCode.UNKNOWN.name, ResultCode.UNKNOWN.name)
+//                throw CollectcardException(ResultCode.UNKNOWN.name, ResultCode.UNKNOWN.name)
+                logger.error("Response validation exception. currently just logging : {} ", ResultCode.UNKNOWN.name)
             }
 
             if (resultCode == ResultCode.EXTERNAL_SERVER_ERROR) {
-                throw CollectcardException(ResultCode.EXTERNAL_SERVER_ERROR.name, ResultCode.EXTERNAL_SERVER_ERROR.name)
+//                throw CollectcardException(ResultCode.EXTERNAL_SERVER_ERROR.name, ResultCode.EXTERNAL_SERVER_ERROR.name)
+                logger.error("Response validation exception. currently just logging : {} ", ResultCode.EXTERNAL_SERVER_ERROR.name)
             }
 
             if (resultCode == ResultCode.INVALID_ACCESS_TOKEN) {
-                throw CollectcardException(ResultCode.INVALID_ACCESS_TOKEN.name, ResultCode.INVALID_ACCESS_TOKEN.name)
+//                throw CollectcardException(ResultCode.INVALID_ACCESS_TOKEN.name, ResultCode.INVALID_ACCESS_TOKEN.name)
+                logger.error("Response validation exception. currently just logging : {} ", ResultCode.INVALID_ACCESS_TOKEN.name)
             }
 
             if (resultCode == ResultCode.INVALID_USER) {
-                throw CollectcardException(ResultCode.INVALID_USER.name, ResultCode.INVALID_USER.name)
+//                throw CollectcardException(ResultCode.INVALID_USER.name, ResultCode.INVALID_USER.name)
+                logger.error("Response validation exception. currently just logging : {} ", ResultCode.INVALID_USER.name)
             }
 
             if (resultCode == ResultCode.UNKNOWN) {
-                throw CollectcardException(ResultCode.UNKNOWN.name, ResultCode.UNKNOWN.name)
+//                throw CollectcardException(ResultCode.UNKNOWN.name, ResultCode.UNKNOWN.name)
+                logger.error("Response validation exception. currently just logging : {} ", ResultCode.UNKNOWN.name)
             }
         }
     }
