6c0101f08b52d4b08ca0150cfc62f41dc4405233
YeSangKug <54267881+YeSangKug@users.noreply.github.com>
2020-08-21 18:02:32 +0900

    Modify micrometer config (#256)

--- src/main/kotlin/com/rainist/collectcard/config/MicrometerConfig.kt
+++ src/main/kotlin/com/rainist/collectcard/config/MicrometerConfig.kt
@@ -1,12 +1,15 @@
 package com.rainist.collectcard.config
 
 import com.rainist.common.interceptor.StatsUnaryServerInterceptor
+import io.micrometer.core.instrument.Meter
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.config.NamingConvention
 import javax.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
 
 @Configuration
 class MicrometerConfig {
@@ -14,13 +17,30 @@ class MicrometerConfig {
     @Autowired
     lateinit var meterRegistry: MeterRegistry
 
+    @Autowired
+    lateinit var customConvention: CustomConvention
+
     @PostConstruct
     fun init() {
-        meterRegistry.Config().namingConvention(NamingConvention.dot)
+        meterRegistry.Config().namingConvention(customConvention)
     }
 
     @Bean
     fun statsUnaryServerInterceptor(): StatsUnaryServerInterceptor {
         return StatsUnaryServerInterceptor(meterRegistry)
+    }
+}
+
+@Component
+class CustomConvention : NamingConvention {
+
+    @Value("\${spring.profiles.active}")
+    lateinit var activeName: String
+
+    @Value("\${spring.application.name}")
+    lateinit var applicationName: String
+
+    override fun name(name: String, type: Meter.Type, baseUnit: String?): String {
+        return "$activeName.$applicationName.$name"
     }
 }
